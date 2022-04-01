9bb4c0240c240f5fd0eddf147fe6303867416fee
Truxton-Marley <stevenstruxtonm@gmail.com>
2022-04-01 08:49:08 +0200

    Add rough draft to find ips in Juniper Config

---
+++ find_ips.py
@@ -0,0 +1,31 @@
+import re
+
+pat_ips = re.compile("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2}")
+#TODO: get the routing instance and search against BGP Peers / IPs
+#pat_routing_instance = re.compile("")
+cidr_dict = {"31": 2, "30": 4, "29": 8, "28": 16,
+             "27": 32, "26": 64, "25": 128, "24": 256}
+
+sometext = """
+set routing-instances THIS-INST-01 interface ge-0/0/0.231
+This is just some text to parse through
+set interfaces ge-0/0/0 unit 231 family inet address 192.168.32.24/31
+set interfaces ge-0/0/0 unit 232 family inet address 192.168.32.9/30
+set interfaces ge-0/0/0 unit 234 family inet address 192.168.32.17/29
+set interfaces ge-0/0/0 unit 234 family inet address 192.168.32.67/24
+Hey this is some more text down below to test with :)
+"""
+
+ips = re.findall(pat_ips, sometext)
+
+search_commands = []
+
+for ip in ips:
+    address, mask = ip.split("/")
+    octets = address.split(".")
+    subnet = int(octets[3]) - int(octets[3]) % cidr_dict[mask]
+    broadcast = subnet + cidr_dict[mask] - 1
+    search_command = "show configuration | display set | match \"" + (".").join(octets[:-1]) + f".[{subnet}-{broadcast}]\""
+    search_commands.append(search_command)
+
+print(search_commands)
